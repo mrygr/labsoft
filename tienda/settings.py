ddfed3cf831eb7e4f102d9ea7617c37ccf85c7d3
yercin <yer-cin@hotmail.com>
2023-08-23 14:44:52 -0500

    os.path.dirname

--- tienda/settings.py
+++ tienda/settings.py
@@ -146,7 +146,7 @@ if not DEBUG:
     STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
