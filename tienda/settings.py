95bb6038dbb37db112803feb20be643e1837fc33
yercin <yer-cin@hotmail.com>
2023-08-23 00:41:16 -0500

    WhiteNoiseMiddleware

--- tienda/settings.py
+++ tienda/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tienda.urls'
